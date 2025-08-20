0648ae9b9ac76b7984dcd575f9ffac950b66ac0f
sarah <sarahhan.one@gmail.com>
2025-08-20 10:40:46 +0900

    Revert "[sarah] Merge branch 'sarah' into main - remove MJ's doodles"
    This reverts commit 64baee24916d68f19c0ec751f33f562d7b31a058, reversing
    changes made to 1fb3384e9f6b6989cebc3713dec74abee37da0b3.

--- solutions/01_wc-lite.sh
+++ solutions/01_wc-lite.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
  # Mission: 01_wc-lite
@@ -22,25 +21,3 @@
 #    echo "My solution for wc-lite"
 # }
 # mjmjmjasdfmfksdajfksadjbfksjdabflksadbflkjasbdflkjbasdfkjasbdfkjbawkvjbzskldjvbkasjdbvkjasdblvk
-=======
-#!/bin/bash
-
- # Mission: 01_wc-lite
- # 텍스트 파일의 라인 수, 단어 수, 문자 수를 세는 `wc` 명령어의 경량 버전을 구현합니다.
-
- # 요구사항:
- #  - -l: 라인 수 출력
- #  - -w: 단어 수 출력
- #  - -c: 문자 수 출력
- #  - 옵션이 없으면 -l, -w, -c 결과를 모두 출력합니다.
-
-
-#  !! 참가자 솔루션 !!
-# 아래에 다른 참가자의 코드를 지우지 말고, 자신의 코드를 추가하세요.
-# 충돌 발생 시, 다른 사람의 코드는 남겨두고 본인의 코드를 그 아래에 병합해야 합니다.
-
-# --- 참가자: [Your Name Here] ---
-# (여기에 자신의 스크립트 코드를 작성하세요)
- wc_lite_solution_yourname() {
-    echo "Sarah's solution for wc-lite"
- }
